94aee3a9715799f23c2a3a0b06b53171eaeed39d
Kirushan Rasendran <kirushan22@gmail.com>
2015-10-10 21:19:13 +0530

    Dribbble Shots and Python hard way course work added

---
+++ Learn python the hardway/Ex13.py
@@ -0,0 +1,12 @@
+#import means adding a new feature to python feature(real name module) set
+from sys import argv
+
+
+#argv is the argument variable
+script,first,second,third=argv
+
+print "The script is called:",script
+print "Your first variable is:",first
+print "Your second variable is:",second
+print "Your third variable is:",third
+
