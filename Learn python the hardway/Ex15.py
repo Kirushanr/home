20904e3ad84e31d5289fcc78561797cecda28ab8
Kirushan Rasendran <kirushan22@gmail.com>
2015-10-11 20:58:03 +0530

    Completed ex 13-15

---
+++ Learn python the hardway/Ex15.py
@@ -0,0 +1,19 @@
+from sys import argv
+
+#pass the file name as commandline argument
+script , filename=argv
+
+#open the file and the file object stored 
+#inside the variable txt
+txt = open(filename)
+
+print "Here is your file %r: " % filename
+print txt.read()
+
+print "Type the filename again: "
+file_again=raw_input("> ")
+
+#open the file and the file object stored 
+#inside the variable
+txt_again =open(file_again)
+print txt_again.read()
